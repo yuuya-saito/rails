d8547c1fa9098f6b3d2d38cfeca3e9246b3bf95f
yuya-ar <yuuya9.8@gmail.com>
2020-09-15 18:49:14 +0900

    初めてのコミット

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get '/' => "home#top"
+  get '/about' => "home#about"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
